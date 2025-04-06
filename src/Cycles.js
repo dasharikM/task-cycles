d499fd2aaaf977023ec973f2705a824807f3c94d
tomoye <kriksnonsi@gmail.com>
2025-04-06 16:07:48 +0300

    Задание с циклами и условными операторами

--- src/Cycles.js
+++ src/Cycles.js
@@ -3,18 +3,43 @@
   Используя цикл for, просуммируйте все четные числа в диапазоне между этими значениями (включительно)
   и верните итоговый результат.
 */
-export function rangeSum(start, end) {}
+export function rangeSum(start, end) {
+    let sum = 0;
+    for (let i = start; i <= end; i += 1) {
+        if (i % 2 == 0) sum += i;
+    }
+    return sum;
+}
 
 /*
   В функцию iterationCount() приходит неотрицательное число.
   Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
   и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
 */
-export function iterationCount(a) {}
+export function iterationCount(a) {
+    let cnt = 0;
+    while (a > 0.1) {
+        a /= 2;
+        cnt += 1;
+    }
+    return cnt;
+}
 
 /*
   В функцию symbolsReplace() приходит строка текста.
   Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
   и верните итоговый результат.
 */
-export function symbolsReplace(message) {}
+export function symbolsReplace(message) {
+    let i = 0;
+    let new_stringa = '';
+    do {
+        if ((i + 1) % 3 == 0) {
+            new_stringa += '_';
+        } else {
+            new_stringa += message[i];
+        }
+        i += 1;
+    } while (i != message.length);
+    return new_stringa;
+}
